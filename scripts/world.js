a5b75e4b3783c48466fe00651cb4172b82832641
Michael <mike_visualsoft@yahoo.com>
2014-12-30 12:31:59 +0700

    Fix Bug

--- scripts/world.js
+++ scripts/world.js
@@ -5,7 +5,14 @@ var zero = {
     running: false,
     jumping:false,
     right : true,
-    getPosX: function () {
+    getPosX_zero: function () {
+        var style = window.getComputedStyle(this.el),
+            left  = style.left
+            posX  = parseInt(left.substr(0, left.length - 2), 10);
+            console.log(posX);
+        return posX;
+    },
+    getPosX_bg: function () {
         var style = window.getComputedStyle(this.el2),
             left  = style.backgroundPosition,
             posX  = parseInt(left.substr(0, left.length - 7), 10);
@@ -26,171 +33,138 @@ var zero = {
             this.el.classList.remove("zero-run-left");
             this.el.classList.remove("zero-jump");
             this.el2.classList.remove("bg-move-right");
+            this.el2.classList.remove("bg-move-left");   
             this.running = false;
             this.jumping = false;
             this.transformed = false;
             this.el.classList.add("zero-stand");
-            var posX   = this.getPosX(),
-            newPos = posX - 5;
-            this.el2.style.backgroundPosition = newPos + "px " +"-3px" ;
         }
-        /*if (this.running) {
-            this.el.style.webkitAnimation = "";
-            this.el.classList.remove("zero-run-right");
-            this.el.classList.remove("zero-run-left");
-            this.el.classList.remove("zero-jump");
-            this.el.classList.add("zero-stand");
-            this.running = false;
-            this.transformed = false;
-        }
-        if(this.jumping){
-            this.el.style.webkitAnimation="";
-            this.el.classList.remove("zero-run-right");
-            this.el.classList.remove("zero-run-left");
-            this.el.classList.remove("zero-jump");
-            this.el.classList.add("zero-down");
-            this.running = false;
-            this.jumping = false;
-            this.transformed = false;
-        }
-        var posY = this.getPosY();
-        console.log(posY);
-        if(posY==145)
-        {
-            this.el.style.webkitAnimation = "";
-            this.el.classList.remove("zero-down");
-            this.el.classList.add("zero-stand");
-        }*/
     },
     moveRight: function () {
-        var posX   = this.getPosX(),
-            newPos = posX - 10;
-
+        var posX_bg   = this.getPosX_bg(),
+            newPos = posX_bg - 10,
+            posX_zero = this.getPosX_zero(),
+            newPoss = posX_zero +3;
+        if(newPos <=-7040)
+        {
+            return;
+        }
+        if(newPoss>=200)
+        {
+            newPoss=200;
+        }
         if (!this.running) {
             this.el.classList.remove("zero-stand");
             this.el.classList.remove("zero-run-left");
-            this.el.webkitAnimation=""; 
+            this.el.webkitAnimation="";
             this.el2.classList.add("bg-move-right");   
             this.el.classList.add("zero-run-right");
             this.running = true; 
             this.right = true;
-        }
-         
-        if (!this.transformed) {
-            this.el.style.webkitTransform = "";
-            this.transformed = !this.transformed;
-        }
-        this.el2.style.backgroundPosition = newPos + "px " +"-3px" ;
-        /*var posX   = this.getPosX(),
-            newPos = posX + 3;
-        if (posX >=998) {
-                posX=1000;
-                newPos=1000;
-        }
-        if (!this.running) {
-            this.el.classList.remove("zero-stand");
-            this.el.classList.remove("zero-run-left");
-            this.el.webkitAnimation="";    
-            this.el.classList.add("zero-run-right");
-            this.running = true; 
-            var posX   = this.getPosX();
-            if (posX >=998) {
-                posX=1000;
-                newPos=1000;
+            /*function findKeyframesRule(rule)
+            {
+                // gather all stylesheets into an array
+                var ss = document.styleSheets;
+                console.log(document.styleSheets[0]);
+                // loop through the stylesheets
+                for (var i = 0; i < ss.length; ++i) {
+                    
+                    // loop through all the rules
+                    for (var j = 0; j < ss[i].length; ++j) {
+                        
+                        // find the -webkit-keyframe rule whose name matches our passed over parameter and return that rule
+                        if (ss[i].cssRules[j].type == window.CSSRule.WEBKIT_KEYFRAMES_RULE && ss[i].cssRules[j].name == rule)
+                            return ss[i].cssRules[j];
+                    }
+                }
+                
+                // rule not found
+                return null;
             }
-            this.right = true;
+            var change = function()
+            {
+                // find our -webkit-keyframe rule
+                var keyframes = findKeyframesRule("bg-right");
+                
+                // remove the existing 0% and 100% rules
+                // keyframes.deleteRule("0%");
+                // keyframes.deleteRule("100%");
+                
+                // create new 0% and 100% rules with random numbers
+                //keyframes.insertRule("0% from { background-position: " + newPos-1000 + " px -3px; }");
+                //keyframes.insertRule("100% from { background-position: " + (newPos-100) + " px -3px; }");
+                // assign the animation to our element (which will cause the animation to run)
+                //document.getElementById('viewport').style.webkitAnimationName = anim;
+            }
+        change();*/
         }
-        
         if (!this.transformed) {
             this.el.style.webkitTransform = "";
             this.transformed = !this.transformed;
         }
-        this.el.style.left = newPos + "px";*/
+        this.el.style.left = newPoss + "px";
     },
     moveLeft: function () {
-         var posX   = this.getPosX(),
-            newPos = posX + 10;
-
+         var posX   = this.getPosX_bg(),
+            newPos = posX + 10,
+            posX_zero = this.getPosX_zero(),
+            newPoss = posX_zero -6;
+        if(newPos >=-6570)
+        {
+            return;
+        }
+        if(newPoss<=3)
+        {
+            newPoss=3;
+        }
         if (!this.running) {
             this.el.classList.remove("zero-stand");
             this.el.classList.remove("zero-run-right");
             this.el.webkitAnimation=""; 
-            this.el2.classList.add("bg-move-right");   
+            //this.el2.classList.add("bg-move-left");   
             this.el.classList.add("zero-run-left");
             this.running = true; 
+            this.right = false;
         }
          
         if (!this.transformed) {
              this.el.style.webkitTransform = "rotateY(180deg)";
             this.transformed = !this.transformed;
         }
-        this.el2.style.backgroundPosition = newPos + "px " +"-3px" ;
-        /*var posX   = this.getPosX(),
-            newPos = posX - 3;
-        
-        if (posX <= 0) {
-            posX=0;
-            newPos=0;
-        }
-        
-        if (!this.running) {
-            this.el.classList.remove("zero-stand");
-            this.el.classList.remove("zero-run-right");
-            this.el.style.webkitAnimation = "";
-            this.el.classList.add("zero-run-left");
-            this.running = true;
-            var posX   = this.getPosX();
-            if (posX <=0) {
-                posX=0;
-                newPos=0;
-            }
-            this.right = false;
-        }
-        if (!this.transformed) {
-            
-            this.el.style.webkitTransform = "rotateY(180deg)";
-            this.transformed = !this.transformed;
-        }
-        this.el.style.left = newPos + "px";*/
+        this.el.style.left = newPoss + "px";
         
     },
     moveUp:function(){
-        var posY = this.getPosY();
+        var down=false;
+        var posY = this.getPosY(),
+            newPos = posY-3;
         console.log(posY);
-        if(posY<=8)
-        {
-            if (this.jumping) {
-                this.el.classList.remove("zero-stand");
-                this.el.classList.remove("zero-jump");
-                this.el.style.webkitAnimation = "";
-                this.el.classList.add("zero-down");
-                this.el.classList.add("zero-stand");
-            }
+        if(newPos<=146){
+            down=true;
+            newPos = 146;
         }
-        else
-        {
-            if (!this.jumping) {
+        else{
+            down = false;
+        }
+        console.log(down);
+        if(!this.jumping){
+            if(!down){
                 this.el.classList.remove("zero-stand");
-                this.el.classList.remove("zero-down")
+                this.el.classList.remove("zero-down");
                 this.el.style.webkitAnimation = "";
                 this.el.classList.add("zero-jump");
                 this.jumping = true;
-            }
-        }
-        /*if (!this.running) {
-            this.el.classList.remove("zero-stand");
-            this.el.style.webkitAnimation = "";
-            this.el.classList.add("zero-jump");
-            //var posY = this.getPosY();
-            console.log(posY);
-            if(posY <=0)
-            {
+                this.running = true;
+            } else {
+                this.el.classList.remove("zero-stand");
                 this.el.classList.remove("zero-jump");
                 this.el.style.webkitAnimation = "";
-                this.el.classList.add("zero-down");
+                //this.el.classList.add("zero-down");
+                this.jumping = false;
+                this.running = false;
             }
-            this.running = true;
-        }*/
+        }
+        //this.el.style.top = newPos + "px";
         if (!this.transformed) {
             this.el.style.webkitTransform = "";
             if(!this.right)
@@ -199,32 +173,6 @@ var zero = {
                 this.el.style.webkitTransform = "rotateY(360deg)";
             this.transformed = !this.transformed;
         }
-        this.el.style.top = posY + "px";
-        if(posY<=8)
-        {
-            if (this.jumping) {
-                this.el.classList.remove("zero-stand");
-                this.el.classList.remove("zero-jump");
-                this.el.style.webkitAnimation = "";
-                this.el.classList.add("zero-down");
-                this.jumping = false;
-            }
-        }
-        else
-        {
-            if (!this.jumping) {
-                this.el.classList.remove("zero-stand");
-                this.el.classList.remove("zero-down");
-                this.el.style.webkitAnimation = "";
-                this.el.classList.add("zero-jump");
-                this.jumping = true;
-            }
-        }
-        this.el.style.top = 145 + "px";
-        this.el.style.webkitAnimation = "";
-        
-        this.el.classList.remove("zero-down");
-        this.el.classList.add("zero-stand");
     }
 };
 
