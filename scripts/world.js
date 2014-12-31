10ec6ea81213b6818c26fbdb80cf4e623ab1d8e4
Michael <mike_visualsoft@yahoo.com>
2014-12-31 11:36:26 +0700

    Edit Background

--- scripts/world.js
+++ scripts/world.js
@@ -1,39 +1,43 @@
 var websocket = new WebSocket("ws://localhost:8080/"),
     zero = {
     el: document.getElementById("zero"),
+    el2 : document.getElementById("viewport"),
     transformed: false,
     running: false,
     jumping :false,
     jumpdown :false,
-    clicked : false,
+    hasJump : false,
     riding : false,
-    facingright : true,
+    right : true,
+    getPosX_bg:function(){
+        var style = window.getComputedStyle(this.el2),
+            left  = style.backgroundPosition,
+            posX  = parseInt(left.substr(0, left.length - 7), 10);
+        return posX;
+    },
     getPosX: function () {
         var style = window.getComputedStyle(this.el),
             left  = style.left,
             posX  = parseInt(left.substr(0, left.length - 2), 10);
-        
         return posX;
     },
     getPosY:function(){
-        var style2 = window.getComputedStyle(this.el),
-            top    = style2.top,
+        var style = window.getComputedStyle(this.el),
+            top    = style.top,
             posY   = parseInt(top.substr(0,top.length - 2),10);
-
         return posY;
     },
-    standstill3 : function(){
-            /*if (right) {
-                this.el.style.left = (this.getPosX()+25) + "px";
-            };*/
+    standStillride : function(){
             this.el.style.width = "55px";
             this.el.classList.remove("zero-ride-right");
             this.el.classList.remove("zero-ride-left");
+            this.el2.classList.remove("bg-move-right"); 
             this.riding = false;
     },
-    standStill2 : function(){
+    standStilldown : function(){
         if (this.jumpdown) {
             this.el.classList.remove("zero-jump-down");
+            //this.el2.classList.remove("bg-move-right");  
             this.el.classList.add("zero-stand");
             this.jumpdown = false;
         }
@@ -41,25 +45,37 @@ var websocket = new WebSocket("ws://localhost:8080/"),
     standStill: function () {
             this.el.classList.remove("zero-run-right");
             this.el.classList.remove("zero-run-left");
+            this.el2.classList.remove("bg-move-right"); 
             this.el.classList.add("zero-stand");
             this.running = false;
     },
     moveRight: function () {
         var posX   = this.getPosX(),
-            newPos;
+            newPos,
+            posX_bg = this.getPosX_bg(),
+            newPoss;
         if (this.riding) {
             newPos = posX + 10;
-            if (newPos >= 545) {
-                newPos = 545;
+            newPoss = posX_bg - 10;
+            if (newPos >= 145) {
+                newPos = 145;
+            }
+            if(newPoss <=-6950)
+            {
+                newPoss = -6800;
             }
         }
         else{
             newPos = posX + 5;
-            if (newPos >= 600) {
-                newPos = 600;
+            newPoss = posX_bg - 10;
+            if (newPos >= 200) {
+                newPos = 200;
+            }
+            if(newPoss <=-6950)
+            {
+                newPoss = -6800;
             }
         }
-
         if(!this.jumping && !this.jumpdown){
             if (!this.running) {
                 this.el.classList.remove("zero-stand");
@@ -69,7 +85,9 @@ var websocket = new WebSocket("ws://localhost:8080/"),
                 }
             }
             else{
-                this.el.classList.add("zero-run-right");   
+                this.el.classList.add("zero-run-right");
+                if(newPos >=200) 
+                    this.el2.classList.add("bg-move-right");  
             }
         }
         else{
@@ -80,24 +98,21 @@ var websocket = new WebSocket("ws://localhost:8080/"),
             this.el.style.webkitTransform = "";
             this.transformed = !this.transformed;
         }
-
         this.el.style.left = newPos + "px";
+        //this.el2.style.backgroundPosition = newPoss+"px -3px";
     },
     moveLeft: function () {
         var posX   = this.getPosX(),
-            newPos;
-        
+            newPos;   
         if (this.riding) {
             newPos = posX - 10;
         }
         else{
             newPos = posX - 5;
         }
-
         if (newPos <= 0) {
             newPos = 0;
         }
-
         if(!this.jumping && !this.jumpdown){
             if (!this.running) {
                 this.el.classList.remove("zero-stand");
@@ -112,31 +127,26 @@ var websocket = new WebSocket("ws://localhost:8080/"),
         }
         else{
             this.el.classList.remove("zero-run-left");
-        }
-        
+        }    
         if (!this.transformed) {
             this.el.style.webkitTransform = "rotateY(180deg)";
             this.transformed = !this.transformed;
         }
-        
         this.el.style.left = newPos + "px";
     },
-    jump: function(){
+    jumpUp: function(){
         var posY   = this.getPosY(),
             newPos = posY - 5;
-
-        this.el.style.height = "55px";
-        if (newPos <= 80) {
+        if (newPos <= 70) {
             clearInterval(t);
-            newPos = 80;
-            u = setInterval(function(){zero.jump_down()},25);
+            newPos = 70;
+            u = setInterval(function(){zero.jumpDown()},25);
             this.el.classList.remove("zero-jump-up");
             this.el.classList.add("zero-jump-down");
         }
-
         if(!this.jumping){
             if(this.riding){
-                this.standstill3();
+                this.standStillride();
             }
             this.el.classList.remove("zero-stand");
             this.el.classList.add("zero-jump-up");
@@ -144,15 +154,12 @@ var websocket = new WebSocket("ws://localhost:8080/"),
         }
         this.el.style.top = newPos + "px";
     },
-    jump_down:function(){
+    jumpDown:function(){
         var posY   = this.getPosY(),
             newPos = posY + 5;
-
-        this.el.style.height = "80px";
-        if (newPos >= 120) {
-            this.clicked = false;
-            this.el.style.height = "50px";
-            newPos = 145;      
+        if (newPos >= 110) {
+            this.hasJump = false;
+            newPos = 145;   
             this.el.style.top = newPos + "px";
             ProsesKeyUp();
             clearInterval(u);
@@ -166,95 +173,78 @@ var websocket = new WebSocket("ws://localhost:8080/"),
         }
         this.el.style.top = newPos + "px";
     }
-},
-t,u,
-jump_sound = new Audio('/javascript/SE/jump1.ogg'),
-ride_sound = null;
-
+};
 
 var keypressed = {};    
  
 window.addEventListener('keydown', function(e) {
     keypressed[e.keyCode] = true;
-}, true);    
+});
  
 window.addEventListener('keyup', function(e) {
     keypressed[e.keyCode] = false;
     ProsesKeyUp();
-},true);
+});
 
-var update = function() {
+var ProsesKeyDown = function() {
     if (keypressed[37]) {
-        zero.facingright = false;
+        zero.right = false;
         zero.running = true;
         if (!zero.riding) {
             zero.moveLeft();
         }
-        // websocket.send("left");
+        websocket.send("left");
     }
     if (keypressed[38]) {
-        if(!zero.clicked && !zero.riding){
+        if(!zero.hasJump && !zero.riding) {
             ProsesKeyUp();
-            jump_sound.play();
-            zero.clicked = true;
+            zero.hasJump = true;
             zero.el.classList.remove("zero-stand");
             zero.el.classList.add("zero-jump-up");
-            t = setInterval(function(){zero.jump()},25);
+            t = setInterval(function(){zero.jumpUp()},25);
         }
-        // websocket.send("jump");
+        websocket.send("jump");
     }  
     if (keypressed[39]) {
-        zero.facingright = true;
+        zero.right = true;
         zero.running = true;
         if (!zero.riding) {
             zero.moveRight();
         }
-        
-        // websocket.send("right");
+        websocket.send("right");
     }  
-    if (keypressed[90]) {
+    /*if (keypressed[90]) {
         if(!zero.running && !zero.jumping && !zero.jumpdown){
             zero.riding = true;
-            if(zero.facingright){
-                if (ride_sound == null) {
-                    ride_sound = new Audio('/javascript/SE/wind2.ogg');
-                    ride_sound.volume = 0.5;
-                    ride_sound.play();
-                }
+            if(zero.right){
                 if (zero.getPosX() >= 545) {
                     zero.el.style.left = "545px";
                 }
                 zero.moveRight();
             }
             else{
-                if (ride_sound == null) {
-                    ride_sound = new Audio('/javascript/SE/wind2.ogg');
-                    ride_sound.volume = 0.5;
-                    ride_sound.play();
-                }
                 zero.moveLeft();
             }
         }
-    }    
-    setTimeout(function(){update()}, 16);
+    }*/   
+    setTimeout(function(){ProsesKeyDown()}, 16);
 }    
  
-update();
+ProsesKeyDown();
 
 var ProsesKeyUp = function () {
     if (zero.running) {
         zero.standStill();
     }
     if (zero.riding == true) {
-        zero.standstill3();
-        ride_sound = null;
+        zero.standStillride();
     };
     if (zero.jumpdown == true) {
         if (!zero.clicked) {
-            zero.standStill2();
+            zero.standStilldown();
         };
     };
-    // websocket.send("stand");
+    websocket.send("stand");
 };
 
 websocket.onmessage = function (message) {
@@ -277,21 +267,21 @@ websocket.onmessage = function (message) {
         case "jump":
             if(!zero.clicked){
                 ProsesKeyUp();
-                zero.clicked = true;
+                zero.hasJump = true;
                 zero.el.classList.remove("zero-stand");
                 zero.el.classList.add("zero-jump-up");
-                t = setInterval(function(){zero.jump()},25);
+                t = setInterval(function(){zero.jumpUp()},25);
             }
         case "stand":
              if (zero.running == true) {
                 if (zero.riding == true) {
-                    zero.standstill3();
+                    zero.standStillride();
                 };
                 zero.standStill();
             }
             if (zero.jumpdown == true) {
                 if (!zero.clicked) {
-                    zero.standStill2();
+                    zero.standStilldown();
                 };
             };
             break;
@@ -307,15 +297,13 @@ websocket.onmessage = function (message) {
         case "watch":
             var wm = document.querySelector("h1#watch");
             wm.innerHTML = "Watch Mode";
-
             window.removeEventListener('keydown', function(e) {
                 keypressed[e.keyCode] = true;
-            }, true);    
- 
+            });    
             window.removeEventListener('keyup', function(e) {
                 keypressed[e.keyCode] = false;
                 ProsesKeyUp();
-            },true);
+            });
             break;
     }
 };
